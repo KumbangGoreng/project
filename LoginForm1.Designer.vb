1bd1080675bc1647137594b949355da557fe7342
Nad <Nad@DESKTOP-4G44E9H>
2023-11-05 21:39:23 +0800

    Add project files.

---
+++ LoginForm1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726")> _
+Partial Class LoginForm1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+    Friend WithEvents LogoPictureBox As System.Windows.Forms.PictureBox
+    Friend WithEvents UsernameLabel As System.Windows.Forms.Label
+    Friend WithEvents PasswordLabel As System.Windows.Forms.Label
+    Friend WithEvents UsernameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents PasswordTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents LoginButton As System.Windows.Forms.Button
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        LogoPictureBox = New PictureBox()
+        UsernameLabel = New Label()
+        PasswordLabel = New Label()
+        UsernameTextBox = New TextBox()
+        PasswordTextBox = New TextBox()
+        LoginButton = New Button()
+        CType(LogoPictureBox, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' LogoPictureBox
+        ' 
+        LogoPictureBox.Image = My.Resources.Resources.A_safer_antibiotic
+        LogoPictureBox.Location = New Point(0, 0)
+        LogoPictureBox.Name = "LogoPictureBox"
+        LogoPictureBox.Size = New Size(165, 193)
+        LogoPictureBox.SizeMode = PictureBoxSizeMode.StretchImage
+        LogoPictureBox.TabIndex = 0
+        LogoPictureBox.TabStop = False
+        ' 
+        ' UsernameLabel
+        ' 
+        UsernameLabel.Location = New Point(172, 24)
+        UsernameLabel.Name = "UsernameLabel"
+        UsernameLabel.Size = New Size(220, 23)
+        UsernameLabel.TabIndex = 0
+        UsernameLabel.Text = "&User name"
+        UsernameLabel.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' PasswordLabel
+        ' 
+        PasswordLabel.Location = New Point(172, 81)
+        PasswordLabel.Name = "PasswordLabel"
+        PasswordLabel.Size = New Size(220, 23)
+        PasswordLabel.TabIndex = 2
+        PasswordLabel.Text = "&Password"
+        PasswordLabel.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' UsernameTextBox
+        ' 
+        UsernameTextBox.Location = New Point(174, 44)
+        UsernameTextBox.Name = "UsernameTextBox"
+        UsernameTextBox.Size = New Size(220, 23)
+        UsernameTextBox.TabIndex = 1
+        ' 
+        ' PasswordTextBox
+        ' 
+        PasswordTextBox.Location = New Point(174, 101)
+        PasswordTextBox.Name = "PasswordTextBox"
+        PasswordTextBox.PasswordChar = "*"c
+        PasswordTextBox.Size = New Size(220, 23)
+        PasswordTextBox.TabIndex = 3
+        ' 
+        ' LoginButton
+        ' 
+        LoginButton.Location = New Point(236, 157)
+        LoginButton.Name = "LoginButton"
+        LoginButton.Size = New Size(94, 23)
+        LoginButton.TabIndex = 4
+        LoginButton.Text = "&Login"
+        ' 
+        ' LoginForm1
+        ' 
+        AcceptButton = LoginButton
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(401, 192)
+        Controls.Add(LoginButton)
+        Controls.Add(PasswordTextBox)
+        Controls.Add(UsernameTextBox)
+        Controls.Add(PasswordLabel)
+        Controls.Add(UsernameLabel)
+        Controls.Add(LogoPictureBox)
+        FormBorderStyle = FormBorderStyle.FixedDialog
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "LoginForm1"
+        SizeGripStyle = SizeGripStyle.Hide
+        StartPosition = FormStartPosition.CenterParent
+        Text = "LoginForm1"
+        CType(LogoPictureBox, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+
+    End Sub
+
+End Class
